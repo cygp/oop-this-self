c4fb8e8a8e39563b9296e57c28eb4e0c2d57b036
cygp <41449855+cygp@users.noreply.github.com>
2018-09-07 14:58:08 +0200

    Change script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
 (function() {
 	function Button(text) {
-    this.text = text || 'Hello';
+		this.text = text || 'Hello';
 	}
-  Button.prototype = {
-    create: function() {
-      var self = this;
-      this.element = document.createElement('button');
-      this.element.innerText = this.text;
-      this.element.addEventListener('click', function() {
-        alert(self.text);
-      });
-      document.body.appendChild(this.element);
-    }
-  }
-  var btn1 = new Button('Hello!');
-  var btn2 = new Button('Hello Moto!');
-  btn1.create();
-  btn2.create();
+	Button.prototype = {
+		create: function() {
+      		var self = this;
+      		this.element = document.createElement('button');
+      		this.element.innerText = this.text;
+      		this.element.addEventListener('click', function() {
+        		alert(self.text);
+      		});
+      		document.body.appendChild(this.element);
+    		}
+  	}
+	var btn1 = new Button('Hello!');
+	var btn2 = new Button('Hello Moto!');
+	btn1.create();
+	btn2.create();
 })();
